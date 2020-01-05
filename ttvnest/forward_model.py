3da5c9f94f74ebf34732185259015ddc3bc5c1c1
Shreyas Vissapragada <svissapr@caltech.edu>
2020-01-05 03:35:20 -0600

    updating files but note that forward_model and retrieval are deprecated now

--- ttvnest/forward_model.py
+++ ttvnest/forward_model.py
@@ -3,29 +3,79 @@ import matplotlib.pyplot as plt
 import ttvfast
 from .constants import *
 
-def run_simulation(stellarmass, dt, sim_length, *argv):
-	nplanets = len(argv)
-	if nplanets < 2:
-		print("You need at least 2 planets for a TTV simulation!")
-		return None
-	start = tkep - tref
+def run_simulation(stellarmass, dt, start_time, sim_length, theta,
+	fixed_params, nplanets, transiting):
+
+	start = start_time - tref
 	end = start + sim_length
+
 	system = []
-	for arg in argv:
-		mp_ms, P, ecosw, esinw, M = arg
+	paramv = parse_with_fixed(theta, fixed_params, nplanets)
+	for param in paramv:
+		mp_ms, P, ecosw, esinw, i, Omega, t0 = param
 		e = np.sqrt(ecosw**2 + esinw**2)
 		w = np.arctan2(esinw, ecosw)*180./np.pi
-		planet_params = [mp_ms*earth_per_sun, P, e, 90., 0., w, M]
+		t0 += start
+		M = t0_to_mean_anom(t0, e, w, P, start)
+		planet_params = [mp_ms*earth_per_sun, P, e, i, Omega, w, M]
 		system.append(ttvfast.models.Planet(*planet_params))	
 	results = ttvfast.ttvfast(system, stellarmass, start, dt, end)
 	results = results['positions']
-	planet = np.array(results[0])
-	time = np.array(results[2])
+	planet = np.array(results[0], dtype = 'int')
+	time = np.array(results[2], dtype = 'float')
 	timing_arrays = []
-	for i in range(nplanets):
-		timing_arr = []
-		for plan, t in zip(planet, time):
-			if plan == i and t != -2.0:
-				timing_arr.append(t - start)
-		timing_arrays.append(np.array(timing_arr, dtype = 'float'))
+	for i in transiting:
+		timing_arr = time[planet == i]
+		timing_arr = timing_arr[timing_arr != -2.0]
+		timing_arr -= start
+		timing_arrays.append(timing_arr)
 	return np.array(timing_arrays)
+
+def parse_fixed(params, fixed_params, planet_tag):
+	for fixed in fixed_params:
+		if fixed[0] == planet_tag:
+			params[param_id(fixed[1])] = fixed[2]
+	return params
+
+def t0_to_mean_anom(t0, e, w_deg, P, t_epoch):
+	w = w_deg * np.pi / 180. #omega in radians
+	f_0 = np.pi/2 - w 	#true anomaly at inf conj	
+	E_0 = 2*np.arctan(np.tan(f_0/2) * np.sqrt((1-e)/(1+e)))
+	M_0 = E_0 - e*np.sin(E_0)
+	#above are eccentric and mean anomalies at inf conj
+
+	t_peri = t0 - P*M_0/(2*np.pi)
+	mean_anomaly = 360./P*(t_epoch - t_peri)
+	return mean_anomaly % 360
+
+def parse_with_fixed(theta, fixed_params, nplanets):
+	paramv = []
+	current_ind = 0
+	for i in range(nplanets):
+		theta_init = np.zeros(7) - 1
+		for fixed in fixed_params:
+			if fixed[0] == i:
+				theta_init[param_id(fixed[1])] = fixed[2]
+		remaining = np.argwhere(theta_init == -1).flatten()
+		for ind in remaining:
+			theta_init[ind] = theta[current_ind]
+			current_ind += 1
+		paramv.append(theta_init)
+	return paramv
+
+def param_id(varname):
+	if varname == 'mass':
+		return 0
+	elif varname == 'period':
+		return 1
+	elif varname == 'ecosw':
+		return 2
+	elif varname == 'esinw':
+		return 3
+	elif varname == 'inc':
+		return 4
+	elif varname == 'LongNode':
+		return 5
+	elif varname == 'MeanAnom':
+		return 6
+	return None
--- ttvnest/retrieval.py
+++ ttvnest/retrieval.py
@@ -1,23 +1,35 @@
 import numpy as np
 from . import forward_model as fm
 from . import plot_utils as pu
+from .constants import *
 import dynesty
 from dynesty import utils as dyfunc
 
-def retrieve(nplanets, prior_transform, data, errs, sampler = 'rwalk',
-	bounder = 'multi', pool = None, queue_size = None):
-	ndim = nplanets*5
-	init_live = 100*ndim
+def retrieve(nplanets, prior_transform, fixed_params, data, errs, 
+	sampler = 'rwalk', bounder = 'multi', pool = None, queue_size = None, 
+	dt = 0.1, sim_length = 2000, start_time = tkep, init_live = None, 
+	dlogz_init = 0.1, transiting = None, n_effective = None,
+	maxiter = None, maxcall = None):
+	ndim = nplanets*7 - len(fixed_params)
+	if init_live is None:
+		init_live = 100*ndim
+	if transiting is None:
+		transiting = np.arange(nplanets)
+	periodic = get_periodic_indices(nplanets, fixed_params)
 
 	#the mean anomaly is always periodic, so do this for every planet
-	periodic = [5*i + 4 for i in range(nplanets)]
-	print("Running dynesty with the {0} sampler and {1} bounding...".format(
-		sampler, bounder))
-	dsampler = dynesty.DynamicNestedSampler(log_likelihood, 
-		prior_transform, ndim, logl_args = (data, errs),
+#	periodic = [5*i + 4 for i in range(nplanets)]
+
+
+	print("Running dynesty with the {0} sampler, {1} bounding, and {2} initial live points...".format(
+		sampler, bounder, init_live))
+	dsampler = dynesty.NestedSampler(log_likelihood, 
+		prior_transform, ndim, logl_args = (data, errs, dt, start_time,
+		sim_length, fixed_params, nplanets, transiting),
 		sample = sampler, bound = bounder, periodic = periodic,
-		pool = pool, queue_size = queue_size)
-	dsampler.run_nested(wt_kwargs = {'pfrac':1.0}, nlive_init = init_live)
+		pool = pool, queue_size = queue_size, nlive = init_live)
+
+	dsampler.run_nested(dlogz = dlogz_init)
 	dresults = dsampler.results
 
 	return dresults
@@ -49,21 +61,18 @@ def posterior_summary(results, filename = None, equal_samples = False):
 		f.close()	
 	return filename
 
-def log_likelihood(theta, data, errs, dt = 0.1, sim_length = 2000):
+def log_likelihood(theta, data, errs, dt, start_time, sim_length, fixed_params,
+	nplanets, transiting):
 	#first check that the dimensions are correct
-	nplanets = int(len(theta)/5)
-	if not (nplanets == len(data) == len(errs)):
-		err = "The number of planets is different than the number of \
-			datasets!"
-		raise ValueError(err)
-
 	try:
 		#then unpack the parameters
 		stellarmass = 1.
-		paramv = [theta[i*5: (i + 1)*5] for i in range(nplanets)]
+		#paramv = [theta[i*5: (i + 1)*5] for i in range(nplanets)]
 
 		#then calculate the model
-		models = fm.run_simulation(stellarmass, dt, sim_length, *paramv)
+		models = fm.run_simulation(stellarmass, dt, start_time,
+				sim_length, theta, fixed_params, nplanets,
+				transiting)
 
 		#then discover the epoch arrays
 		epochs = np.array([get_inds(model, datum) for model, datum in \
@@ -73,19 +82,32 @@ def log_likelihood(theta, data, errs, dt = 0.1, sim_length = 2000):
 		#(this accounts for observation gaps)
 		models = np.array([model[epoch] for model, epoch in \
 			zip(models, epochs)])
+		"""
+		import matplotlib.pyplot as plt
+		datum_trend = pu.get_trend(data[0], epochs[0], errs[0])
+		ttv_model = models[0] - datum_trend(epochs[0])
+		plt.plot(epochs[0], ttv_model)
+		plt.plot(epochs[0], data[0] - datum_trend(epochs[0]), 'ko')
+		plt.xlabel("Epoch")
+		plt.ylabel("TTV [day]")
+		print(data[0], models[0])
+		print(datum_trend(epochs[0]))
+		print(theta)
+		plt.show()
+		"""
 	except ValueError as e:
 		return -1e300
 
 	#then calculate log likelihood sum
-	log_likelihood = 0
+	log_like = 0
 	for model, datum, err in zip(models, data, errs):
 		residsq = (model - datum)**2 / err**2
-		log_likelihood -= 0.5 * np.sum(residsq + np.log(2*np.pi*err**2))
+		log_like -= 0.5 * np.sum(residsq + np.log(2*np.pi*err**2))
 
-	if not np.isfinite(log_likelihood):
+	if not np.isfinite(log_like):
 		return -1e300
 	
-	return log_likelihood
+	return log_like
 
 def find_nearest(array, value):
 	return (np.abs(array - value)).argmin()
@@ -93,3 +115,15 @@ def find_nearest(array, value):
 def get_inds(modeled, observed):
 	idxarr = [find_nearest(modeled, val) for val in observed]
 	return idxarr
+
+def get_periodic_indices(nplanets, fixed_params):
+	#LongNode and MeanAnom are always periodic
+	allinds = np.zeros(7*nplanets)
+	for i in range(nplanets):
+		allinds[7*i + 5] = 1 #LongNode
+#		allinds[7*i + 6] = 1 #MeanAnom
+	delinds = []
+	for fixed in fixed_params:
+		delinds.append(fixed[0]*7+fm.param_id(fixed[1]))
+	allinds = np.delete(allinds, delinds)
+	return np.argwhere(allinds == 1).flatten()
