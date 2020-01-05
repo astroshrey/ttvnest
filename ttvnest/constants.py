ce3a939a489cc6b57cbebb135fa2479a18572518
Shreyas Vissapragada <svissapr@caltech.edu>
2020-01-05 03:34:19 -0600

    updating constants file

--- ttvnest/constants.py
+++ ttvnest/constants.py
@@ -1,4 +1,4 @@
 tref = 2456000 #from Deck+14, in orig. TTVFast
-tkep = 2454900
+tkep = 2454900	#kepler reference
+ttess = 2457000	#tess reference
 earth_per_sun = 3e-6 #earth mass per solar mass
-n_comment_lines = 39 #for the header of the Holczer catalog
