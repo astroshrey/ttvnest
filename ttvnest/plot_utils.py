1a2caac755ec611cc07fc024dee7afd09f12e652
Shreyas Vissapragada <svissapr@caltech.edu>
2020-01-05 03:41:59 -0600

    updating plotting stuff for new interface

--- ttvnest/plot_utils.py
+++ ttvnest/plot_utils.py
@@ -12,39 +12,103 @@ matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 matplotlib.rcParams['font.size'] = 20
 
-def gen_labels(nplanets):
-	labels = []
-	for i in range(nplanets):
-		num = str(i + 1)
-		labels += [r'$M_'+num+'/M_\star$',
-			r'$P_'+num+'\ [\mathrm{days}]$', 
-			r'$e_'+num+'\cos(\omega_'+num+')$', 
-			r'$e_'+num+'\sin(\omega_'+num+')$',
-			r'$\mathcal{M}_'+num+'\ [^\circ]$']
-	return labels	
+def dynesty_plots(system, truthvals = None, outname = None):
+	if outname is not None:
+		names = [outname + '_cornerplot.png',
+			outname + '_traceplot.png']
 
-def dynesty_plots(dresults, nplanets):
-	labels = gen_labels(nplanets)
-	ndim = len(labels)
-
+	#cornerplot
 	plt.figure(figsize = (20, 20))
-	cfig, caxes = dyplot.cornerplot(dresults, color = 'blue',
-		max_n_ticks = 3, labels = labels)
-	plt.show()
+	cfig, caxes = dyplot.cornerplot(system.results, color = 'blue',
+		max_n_ticks = 3, labels = system.fit_param_names,
+		truths = truthvals)
+	if outname == None:
+		plt.show()
+	else:
+		plt.savefig(names[0])
+		plt.close('all')
 
+	#traceplot
 	plt.figure(figsize = (20, 20))
-	rfig, raxes = dyplot.runplot(dresults)
-	plt.show()
+	tfig, taxes = dyplot.traceplot(system.results, truths = truthvals,
+		show_titles = True, trace_cmap = 'viridis',
+		labels = system.fit_param_names)
+	if outname == None:
+		plt.show()
+		return None
+	else:
+		plt.savefig(names[0])
+		plt.close('all')
+		return names
+
+def plot_results(system, uncertainty_curves = 0, sim_length = 2000,
+	outname = None):
 	
-	plt.figure(figsize = (20, 20))
-	tfig, taxes = dyplot.traceplot(dresults, truths = np.zeros(ndim), 
-		show_titles = True, trace_cmap = 'viridis', labels = labels)
-	plt.show()
+	dresults = system.results
+	samples = dresults.samples
+	weights = np.exp(dresults.logwt - dresults.logz[-1])
+	ind = np.argmax(dresults.logl)
+	max_like_res = dresults.samples[ind]
 
+	samples_equal = dyfunc.resample_equal(samples, weights)
+	inds = np.arange(samples_equal.shape[0])
+	pick = np.random.choice(inds, uncertainty_curves, replace=False)
+	random_samples = samples_equal[pick,:]
+
+	#data and median result
+	system.sim_length = sim_length
+	models = system.forward_model(max_like_res)
+
+	#uncertainty results
+	unc_models = []
+	for samp in random_samples:
+		rand_models = system.forward_model(samp)
+		unc_models.append(rand_models)
+
+	for i, transiting in enumerate(system.transiting):
+		if transiting:
+			pl = system.planets[i]
+			datum = pl.ttv
+			err = pl.ttv_err
+			epoch = pl.epochs
+			model = models[i]
+
+			datum_trend = get_trend(datum, epoch, err)
+			ttv_datum = datum - datum_trend(epoch)
+			ttv_model = model - datum_trend(np.arange(len(model)))
+
+			plt.figure(figsize = (12, 8))
+			plt.errorbar(epoch, ttv_datum*24.*60., 
+				yerr = err*24.*60., linestyle = '', 
+				color = 'k', marker = 'o', ms = 4, zorder = 1)
+			plt.plot(np.arange(len(model)), ttv_model*24.*60.,
+				color = 'blue', linewidth = 1, zorder = 1000)
+
+			for unc_mod in unc_models:
+				unc_model = unc_mod[i]
+				unc_ttv_model = unc_model - datum_trend(
+					np.arange(len(unc_model)))
+				plt.plot(np.arange(len(unc_model)),
+					unc_ttv_model*24.*60., color = 'blue',
+					linewidth = 0.5, alpha = 0.05,
+					zorder = 10)
+				plt.xlim(-1, len(unc_model))
+			plt.xlabel("Epoch")
+			plt.ylabel("TTV [min]")
+			if outname is None:
+				plt.show()
+				plt.close('all')
+			else:
+				plt.savefig(outname + f'_{i}.png')
+				plt.close('all')
 	return None
 
-def plot_results(dresults, data, errs, uncertainty_curves = 0, dt = 0.1,
-	sim_length = 2000):
+'''
+def plot_results(dresults, data, errs, fixed_params, nplanets, 
+	transiting = None, start_time = tkep,
+	uncertainty_curves = 0, dt = 0.1, sim_length = 2000):
+	if transiting == None:
+		transiting = np.arange(nplanets)
 
 	samples = dresults.samples
 	weights = np.exp(dresults.logwt - dresults.logz[-1])
@@ -56,29 +120,29 @@ def plot_results(dresults, data, errs, uncertainty_curves = 0, dt = 0.1,
 	random_samples = samples_equal[pick,:]
 
 	#data and median result
-	n_planets = int(samples.shape[1]/5)
-	paramv = [median_result[i*5:(i+1)*5] for i in range(n_planets)]
-	models = fm.run_simulation(1., dt, sim_length, *paramv)
-	epochs = [ret.get_inds(model, dat) for model, dat in zip(models, data)]
+	models = fm.run_simulation(1., dt, start_time, sim_length,
+		median_result, fixed_params, nplanets, transiting)
+	epochs = np.array([ret.get_inds(model, datum) for model, datum in \
+		zip(models, data)])
 
 	#uncertainty results
 	unc_models = []
 	unc_epochs = []
 	for samp in random_samples:
-		rand_paramv = [samp[i*5:(i+1)*5] for i in range(n_planets)]
-		rand_models = fm.run_simulation(1., dt, sim_length,
-			*rand_paramv)
-		rand_epochs = [ret.get_inds(model, dat) for model, dat in zip(
-			rand_models, data)]
+		rand_models = fm.run_simulation(1., dt, start_time, sim_length,
+			samp, fixed_params, nplanets, transiting)
+		rand_epochs = np.array([ret.get_inds(
+			model, datum) for model, datum in zip(
+			rand_models, data)])
 		unc_epochs.append(rand_epochs)
 		unc_models.append(rand_models)
 
-	for i in range(n_planets):
+	for i in range(nplanets):
 		datum = data[i]
 		model = models[i]
 		err = errs[i]
 		epoch = epochs[i]
-		datum_trend = get_trend(datum, epoch, err, paramv[i][1])
+		datum_trend = get_trend(datum, epoch, err)
 		ttv_datum = datum - datum_trend(epoch)
 		ttv_model = model - datum_trend(np.arange(len(model)))
 		plt.figure(figsize = (12, 8))
@@ -101,6 +165,7 @@ def plot_results(dresults, data, errs, uncertainty_curves = 0, dt = 0.1,
 		plt.show()
 		plt.close('all')
 	return None
+'''
 
 def get_trend(data, obsind, errs, get_uncertainty = False):
 	z = np.polyfit(obsind, data, 1, w = 1/errs)
@@ -148,10 +213,13 @@ def plot_results_with_uncertainty(theta, data, errs, dt = 0.1, sim_length = 2000
 	plot_resid(data, models, errs, epochs, paramv)
 	return None
 
-def plot_ttv_from_params(theta, data, errs, dt = 0.1, sim_length = 2000):
+def plot_ttv_from_params(theta, data, errs, dt = 0.1, start_time = tkep,
+	sim_length = 2000, fixed_params = None, nplanets = None,
+	transiting = None):
 	n_planets = int((len(theta))/5)
 	paramv = [theta[i*5:(i+1)*5] for i in range(n_planets)]
-	models = fm.run_simulation(1., dt, sim_length, *paramv)
+	models = fm.run_simulation(1., dt, start_time, sim_length, theta,
+		fixed_params, nplanets, transiting)
 	epochs = [ret.get_inds(model, dat) for model, dat in zip(models, data)]
 	models = np.array([model[epoch] for model, epoch in \
 		zip(models, epochs)])
@@ -169,6 +237,24 @@ def plot_ttv(data, models, errs, epochs, paramv):
 		plt.xlabel("Time [BJD - 2454900]")
 		plt.ylabel("TTV [min]")
 		plt.show()
+	return None
+
+def plot_ttv_data(system):
+	data = system.data
+	errs = system.errs
+	epochs = system.epochs
+
+	for datum, err, epoch in zip(data, errs, epochs):
+		plt.figure(figsize = (12, 8))
+		datum_trend = get_trend(datum, epoch, err)
+		ttv_datum = datum - datum_trend(epoch)
+		plt.errorbar(epoch, ttv_datum*24.*60., yerr = err*24.*60.,
+			linestyle = '', color = 'k', marker = 'o', ms = 4,
+			zorder = 1)
+		plt.xlabel("Epoch")
+		plt.ylabel("TTV [min]")
+		plt.show()
+		plt.close('all')
 	return None
 
 def plot_resid(data, models, errs, epochs, paramv):
