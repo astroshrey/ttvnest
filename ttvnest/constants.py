0d2d3f3e65a85188f64fbe5aadb17260801bbaa3
Shreyas Vissapragada <svissapr@caltech.edu>
2020-04-20 16:24:34 -0500

    updating some of the simulation constants

--- ttvnest/constants.py
+++ ttvnest/constants.py
@@ -2,3 +2,6 @@ tref = 2456000 #from Deck+14, in orig. TTVFast
 tkep = 2454900	#kepler reference
 ttess = 2457000	#tess reference
 earth_per_sun = 3e-6 #earth mass per solar mass
+e_cutoff = 0.5 #higher eccentricities need better timesteps
+buffersize = 2 #buffer size in days for internal timekeeping
+stellarmass = 1 #everything is in dynamical masses
